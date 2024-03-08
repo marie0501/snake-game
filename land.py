221f21d7e6cb9520b7cb11a5af5b8ff013cf72b4
marie0501 <mariedelvallereyes01@gmail.com>
2024-03-08 15:47:18 -0500

    Initial commit

---
+++ land.py
@@ -0,0 +1,4 @@
+class Land:
+
+    def __init__(self):
+        pass
---
+++ snake.py
@@ -0,0 +1,14 @@
+class Snake:
+
+    def __init__(self, head, body, tail):
+        self.head = head
+        self.body = body
+        self.tail = tail
+
+    def move(self, position):
+        pass
+
+    def eat(self, fruit):
+        pass
+
+    
